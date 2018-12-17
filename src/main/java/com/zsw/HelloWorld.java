170b7c9aeb2581d96bb3019d037ac617d5f27a27
Kay <317876700@qq.com>
2018-12-18 02:24:00 +0800

    测试分支合并

--- src/main/java/com/zsw/HelloWorld.java
+++ src/main/java/com/zsw/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("left......");
 		System.out.println("right.....");
+		System.out.println("master主干合并了hot fix分支");
 	}
 
 }
