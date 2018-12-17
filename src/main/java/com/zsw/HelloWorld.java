7968bdf406e20a81c1f4601f4dbd566e7d091c8d
Kay <317876700@qq.com>
2018-12-18 02:00:57 +0800

    第四次修改Hot fix

--- src/main/java/com/zsw/HelloWorld.java
+++ src/main/java/com/zsw/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("left......");
 		System.out.println("right.....");
+		System.out.println("hot fix");
 	}
 
 }
